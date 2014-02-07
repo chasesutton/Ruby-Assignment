6d13e4311b4e8aaade011e5ab24c4c35d4d34000
Chase Sutton <chasesutton7@gmail.com>
2014-02-06 23:24:25 -0800

    q5 understood

---
+++ q5.rb
@@ -0,0 +1,37 @@
+# (5)  Anagrams
+
+# An anagram is a word obtained by rearranging the letters of another word. For example, "rats", "tars", and "star" are anagrams of one another, as are "dictionary" and "indicatory". We will call any array of single-word anagrams an anagram group. For instance, ["rats", "tars", "star"] is an anagram group, as is["dictionary"].
+
+# Write a method combine_anagrams(words) that, given an array of strings words, groups the input words into anagram groups. Case doesn't matter in classifying strings as anagrams (but case should be preserved in the output), and the order of the anagrams in the groups doesn't matter. The output should be an array of anagram groups (i.e. an array of arrays).
+
+# Code skeleton:
+
+def combine_anagrams(words)
+
+  words.group_by { |word| word.downcase.chars.sort.join}.values
+  									    #chars returns an array of characters from word
+  									    #sorts array from a to z
+  									    #join puts the char array back into a string
+  									    #finally values just puts the array
+
+end
+
+
+
+# # Example test case:
+
+words =  ['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream']
+
+p combine_anagrams(words)
+
+# output: [ ["cars", "racs", "scar"],
+
+#           ["four"],
+
+#           ["for"],
+
+#           ["potatoes"],
+
+#           ["creams", "scream"] ]
+
+# Hint: You can quickly tell if two words are anagrams by sorting their letters, keeping in mind that upper vs. lowercase doesn't matter.
